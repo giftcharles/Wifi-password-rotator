26270b968bc1605257e53249a767f3da500a9f9f
webrahisi@gmail.com <n8181818>
2019-03-11 17:53:07 +0300

    Added send email function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-# generate random passwords
 import random
+import smtplib
+
+# generate random passwords
 
 def GeneratePassword():
     prefix = "Normet2019-"
@@ -12,7 +14,28 @@ def GeneratePassword():
 
     randomPass = prefix + randomLetter + str(number)
 
-    print(randomPass)
+    return randomPass
 
 
-# 
+# Email the password to the admin
+def send_password_via_email():
+    gmail_user = 'giftnakembetwa@gmail.com'  
+    gmail_password = 'n8181818'
+
+    sent_from = gmail_user  
+    to = ['mcrider45g@gmail.com']  
+    subject = 'NEW WIFI PASSWORD'  
+    body = 'The new WIFI password is:\n %s' % (GeneratePassword(),)
+
+    email_text = 'From: %s \nTo: %s\nSubject: %s\n\n %s' % (sent_from, ", ".join(to), subject, body)
+
+    try:  
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+
+        print('Email sent!')
+    except:  
+        print('Something went wrong...')
