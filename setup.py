b494e526a659603e1d1b1ba7aeed872a8b7d043f
giftcharles <mcrider45g@gmail.com>
2019-03-22 16:42:44 +0300

    No console build

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.platform == 'win32':
     base = 'Win32GUI'
 
 includefiles = ["./log/", './GUI/bitmaps/icon-cog.png', './GUI/bitmaps/save-32.png', './GUI/bitmaps/start-green-play-icon-1.png', './GUI/bitmaps/iconfinder_lowercase_letter_x_red_3052270.png', 'icon-green.ico', 'icon-yellow.ico', 'icon-normal.ico', 'icon-normal.png', 'Registry-f2600ffbcc35.json', 'Profile_settings.pkl', 'Last Change Date.dat', 'chromedriver.exe', 'credentials.json']
-executables = [Executable("WPR.py", base=base, icon='icon-normal.ico'), Executable("settings.py", base=None, icon='icon-cog.ico')]
+executables = [Executable("WPR.py", base=base, icon='icon-normal.ico'), Executable("settings.py", base=base, icon='icon-cog.ico')]
 
 packages = ["idna","selenium","random", "time", "smtplib", "codecs", "datetime", "pywifi", "gspread","oauth2client.service_account",
             "_pickle", "infi/systray", "pkg_resources._vendor", "wx", "subprocess", "logging" ]
